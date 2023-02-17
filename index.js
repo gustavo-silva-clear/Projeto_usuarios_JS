9847715623cda7da63b1fe6c7c6ce15fc268e689
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-17 10:20:23 -0300

    feat: starting the users project

---
+++ index.js
@@ -0,0 +1,10 @@
+var nome = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name = gender]:checked")
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var inputFile = document.querySelector("#exampleInputFile");
+//var admim = document.querySelector("#");
+console.log(gender);
+

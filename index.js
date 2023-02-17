c087881423da0ed94b3d78ac537a1545ff7d25f1
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-17 11:12:34 -0300

    feat:separating input information from gender

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ var country = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var inputFile = document.querySelector("#exampleInputFile");
-//var admim = document.querySelector("#");
+var admin = document.querySelector("#admin");
 console.log(gender);
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field , index){
+
+
+    if(field.name == 'gender'){
+
+        user[field.name] = field.value;
+    }
+ 
+    else {
+
+        user[field.name] = field.value;
+
+    }
+
+});
+ 
 

21257b05ce386df403b71ec4047bb99486def2b3
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-02-22 09:57:24 -0300

    feat: adjusting the view and using the ternary operator

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ class UserController {
     constructor(formId , tableId) {
 
         this.formEl = document.getElementById(formId);
-        this.tableId = document.getElementById(tableId);
+        this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
 
@@ -64,8 +64,16 @@ class UserController {
             reject(e);
         };
 
-        fileReader.readAsDataURL(file);
 
+
+        if (file){ 
+        fileReader.readAsDataURL(file);
+        }
+
+        else{
+
+            resolve('/home/gustavonascimento/Downloads/user.png'); 
+        }
         });
 
     }
@@ -82,6 +90,12 @@ class UserController {
                 user[field.name] = field.value;
             }
 
+            else if (field.name === 'admin') 
+            {
+
+        user[field.name] = field.checked;
+
+            }
             else {
 
                 user[field.name] = field.value;
@@ -104,20 +118,22 @@ class UserController {
     }
 
      addLine(dataUser) {
-           
-            this.tableId.innerHTML = `
-             <tr>
+
+        let tr = document.createElement('tr');
+
+        tr.innerHTML  = `
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${(dataUser.admin) ? "Sim": "Não"}</td>
                 <td>${dataUser.birth}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-            </tr>
             `;
+
+            this.tableEl.appendChild(tr);
         }
 
 }
